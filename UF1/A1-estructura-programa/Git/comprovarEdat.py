93043e40772e06b7122889cf90b040a6399e5d81
AdamBenAhmed <adam.benahmed.7e7@itb.cat>
2023-09-22 11:44:02 +0200

    first push

---
+++ UF1/A1-estructura-programa/Git/comprovarEdat.py
@@ -0,0 +1,14 @@
+"""
+Adam Ben Ahmed Belachi
+22/09/2023
+MO3 UF1 A1
+El programa
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+
+if edat>=18:
+    print("Ets major d'edat")
+print("Programa Finalitzat")
