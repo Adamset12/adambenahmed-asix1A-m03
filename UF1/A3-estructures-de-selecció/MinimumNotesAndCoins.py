ece1872d23a1c9759b4f1db007e769151ae28266
AdamBenAhmed <adam.benahmed.7e7@itb.cat>
2023-10-27 09:16:27 +0200

    push to git

--- UF1/A3-estructures-de-selecció/MinimumNotesAndCoins.py
+++ UF1/A3-estructures-de-selecció/MinimumNotesAndCoins.py
@@ -1,71 +1,40 @@
-import decimal
-money = float(input())
 
-values = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]
-efectivo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+try:
+    import decimal
+    money = decimal.Decimal(input())
 
-
-while money > 0:
-    if money - 500 >= 0:
-        money -= 500
-        efectivo[0] += 1
-    elif money - 200 >= 0:
-        money -= 200
-        efectivo[1] += 1
-    elif money - 100 >= 0:
-        money -= 100
-        efectivo[2] += 1
-    elif money - 50 >= 0:
-        money -= 50
-        efectivo[3] += 1
-    elif money - 20 >= 0:
-        money -= 20
-        efectivo[4] += 1
-    elif money - 10 >= 0:
-        money -= 10
-        efectivo[5] += 1
-    elif money - 5 >= 0:
-        money -= 5
-        efectivo[6] += 1
-    elif money - 2 >= 0:
-        money -= 2
-        efectivo[7] += 1
-    elif money - 1 >= 0:
-        money -= 1
-        efectivo[8] += 1
-    elif money - Decimal(0.50) >= 0:
-        money -= Decimal(0.50)
-        efectivo[9] += 1
-    elif money - decimal(0.20) >= 0:
-        money -= decimal(0.20)
-        efectivo[10] += 1
-    elif money - decimal(0.10) >= 0:
-        money -= decimal(0.10)
-        efectivo[11] += 1
-    elif money - decimal(0.05) >= 0:
-        money -= decimal(0.05)
-        efectivo[12] += 1
-    elif money - decimal(0.02) >= 0:
-        money -= decimal(0.02)
-        efectivo[13] += 1
-    elif money - decimal(0.01) >= 0:
-        money -= decimal(0.01)
-        efectivo[14] += 1
-
-print(efectivo[0])
-print(efectivo[1])
-print(efectivo[2])
-print(efectivo[3])
-print(efectivo[4])
-print(efectivo[5])
-print(efectivo[6])
-print(efectivo[7])
-print(efectivo[8])
-print(efectivo[9])
-print(efectivo[10])
-print(efectivo[11])
-print(efectivo[12])
-print(efectivo[13])
-print(efectivo[14])
-
-
+    values = [
+        decimal.Decimal('500'),
+        decimal.Decimal('200'),
+        decimal.Decimal('100'),
+        decimal.Decimal('50'),
+        decimal.Decimal('20'),
+        decimal.Decimal('10'),
+        decimal.Decimal('5'),
+        decimal.Decimal('2'),
+        decimal.Decimal('1'),
+        decimal.Decimal('0.50'),
+        decimal.Decimal('0.20'),
+        decimal.Decimal('0.10'),
+        decimal.Decimal('0.05'),
+        decimal.Decimal('0.02'),
+        decimal.Decimal('0.01')
+    ]
+    efectivo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    i = 0
+    while i in range(len(values)):
+        while money >= values[i]:
+            money -= values[i]
+            efectivo[i] += 1
+        i += 1
+    i = 0
+    while i in range(len(efectivo)):
+        if efectivo[i] != 0:
+            if int(values[i]) > 2 and efectivo[i] > 1: print(str(efectivo[i]) + " bitllets de " + str(values[i]) + "€")
+            elif int(values[i]) > 2 and efectivo[i] == 1: print(str(efectivo[i]) + " bitllet de " + str(values[i]) + "€")
+            elif int(values[i]) > 0.50 and efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + "€")
+            elif int(values[i]) > 0.50 and efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + "€")
+            elif efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + " cèntims")
+            else: print(str(efectivo[i]) + " moneda de " + str(values[i]) + " cèntims")
+        i += 1
+except ValueError or IndexError: print("Pon un valor vàlido")
