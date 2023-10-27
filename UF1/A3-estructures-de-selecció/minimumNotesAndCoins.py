187b4728ba83c7fe34aec85ac4f065d5eeea0ec6
Adamset12 <145340582+Adamset12@users.noreply.github.com>
2023-10-27 20:19:48 +0200

    Update minimumNotesAndCoins.py

--- UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
+++ UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
@@ -5,8 +5,7 @@ try:
     i = 0
     while i < len(money) and money[i] != ".": i += 1
     aux = money[0:i]
-    print(str(float(money) % int(aux)))
-    if len(str(float(money) % int(aux))) > 3: exit(1)
+    if len(str(decimal.Decimal(money) % int(aux))) > 4: exit(1)
     money = float(money)
     values = [float('500'), float('200'), float('100'), float('50'), float('20'), float('10'), float('5'), float('2'), float('1'), float('0.50'), float('0.20'), float('0.10'), float('0.05'), float('0.02'), float('0.01')]
     efectivo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
