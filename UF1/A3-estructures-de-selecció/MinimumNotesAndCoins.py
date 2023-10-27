9ecc192e989fc84f9663cc121c9187101134c3ce
AdamBenAhmed <adam.benahmed.7e7@itb.cat>
2023-10-27 10:40:42 +0200

    push to git

--- UF1/A3-estructures-de-selecció/MinimumNotesAndCoins.py
+++ UF1/A3-estructures-de-selecció/MinimumNotesAndCoins.py
@@ -1,8 +1,11 @@
 
 try:
     import decimal
-    money = decimal.Decimal(input())
-
+    money = input()
+    i = 0
+    while i < len(money) and money[i] != ".": i += 1
+    if float(money) % pow(10, i-1) > 99: exit(1)
+    money = decimal.Decimal(money)
     values = [
         decimal.Decimal('500'),
         decimal.Decimal('200'),
@@ -27,14 +30,14 @@ try:
             money -= values[i]
             efectivo[i] += 1
         i += 1
-    i = 0
-    while i in range(len(efectivo)):
+    i = -1
+    while i in range(len(efectivo)) or i == -1:
         if efectivo[i] != 0:
             if int(values[i]) > 2 and efectivo[i] > 1: print(str(efectivo[i]) + " bitllets de " + str(values[i]) + "€")
             elif int(values[i]) > 2 and efectivo[i] == 1: print(str(efectivo[i]) + " bitllet de " + str(values[i]) + "€")
             elif int(values[i]) > 0.50 and efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + "€")
             elif int(values[i]) > 0.50 and efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + "€")
             elif efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + " cèntims")
-            else: print(str(efectivo[i]) + " moneda de " + str(values[i]) + " cèntims")
+            elif efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + " cèntims")
         i += 1
 except: print("Pon un valor vàlido")
