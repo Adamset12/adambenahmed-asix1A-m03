e878d67ca408bb8d72b321556fdfe32c14e9565c
AdamBenAhmed <adam.benahmed.7e7@itb.cat>
2023-10-30 09:12:39 +0100

    push to git

--- UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
+++ UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
@@ -5,16 +5,20 @@ try:
     i = 0
     while i < len(money) and money[i] != ".": i += 1
     aux = money[0:i]
-    print(str(float(money) % int(aux)))
-    if len(str(float(money) % int(aux))) > 3: exit(1)
+    if len(str(decimal.Decimal(money) % int(aux))) > 4: exit(1)
     money = float(money)
     values = [float('500'), float('200'), float('100'), float('50'), float('20'), float('10'), float('5'), float('2'), float('1'), float('0.50'), float('0.20'), float('0.10'), float('0.05'), float('0.02'), float('0.01')]
     efectivo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     i = 0
-    while i in range(len(values)):
+    '''while i in range(len(values)):
         while money >= values[i]:
             money -= values[i]
             efectivo[i] += 1
+        i += 1'''
+    while i in range(len(values)):
+        if money >= values[i]:
+            efectivo[i] += money // values[i]
+            money -= values[i] * (money //values[i])
         i += 1
     i = -1
     while i in range(len(efectivo)) or i == -1:
