d2a893c0f69c499c3af3d6d102153ad055c0bc7e
AdamBenAhmed <adam.benahmed.7e7@itb.cat>
2023-10-30 11:55:55 +0100

    push to git

--- UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
+++ UF1/A3-estructures-de-selecció/minimumNotesAndCoins.py
@@ -1,4 +1,3 @@
-
 try:
     import decimal
     money = input()
@@ -10,24 +9,18 @@ try:
     values = [float('500'), float('200'), float('100'), float('50'), float('20'), float('10'), float('5'), float('2'), float('1'), float('0.50'), float('0.20'), float('0.10'), float('0.05'), float('0.02'), float('0.01')]
     efectivo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     i = 0
-    '''while i in range(len(values)):
-        while money >= values[i]:
-            money -= values[i]
-            efectivo[i] += 1
-        i += 1'''
     while i in range(len(values)):
         if money >= values[i]:
             efectivo[i] += money // values[i]
             money -= values[i] * (money //values[i])
         i += 1
-    i = -1
-    while i in range(len(efectivo)) or i == -1:
-        if efectivo[i] != 0:
-            if int(values[i]) > 2 and efectivo[i] > 1: print(str(efectivo[i]) + " bitllets de " + str(values[i]) + "€")
-            elif int(values[i]) > 2 and efectivo[i] == 1: print(str(efectivo[i]) + " bitllet de " + str(values[i]) + "€")
-            elif int(values[i]) > 0.50 and efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + "€")
-            elif int(values[i]) > 0.50 and efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + "€")
-            elif efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + " cèntims")
-            elif efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + " cèntims")
+    i = 0
+    while i in range(len(efectivo)):
+        if int(values[i]) > 2 and efectivo[i] > 1: print(str(efectivo[i]) + " bitllets de " + str(values[i]) + "€")
+        elif int(values[i]) > 2 and efectivo[i] == 1: print(str(efectivo[i]) + " bitllet de " + str(values[i]) + "€")
+        elif int(values[i]) > 0.50 and efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + "€")
+        elif int(values[i]) > 0.50 and efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + "€")
+        elif efectivo[i] > 1: print(str(efectivo[i]) + " monedes de " + str(values[i]) + " cèntims")
+        elif efectivo[i] == 1: print(str(efectivo[i]) + " moneda de " + str(values[i]) + " cèntims")
         i += 1
 except: print("Pon un valor vàlido")
