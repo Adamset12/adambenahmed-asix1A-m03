68b1ad1a6f559ee7c485e306dc0e88480862351a
Adamset12 <145340582+Adamset12@users.noreply.github.com>
2023-11-26 14:37:45 +0100

    Update OnlyVowels.py

--- UF1/A4-estructures-de-repetició/OnlyVowels.py
+++ UF1/A4-estructures-de-repetició/OnlyVowels.py
@@ -2,5 +2,5 @@ quantitat = int(input())
 lletres = input().split()
 i = 0
 while i < quantitat:
-    if lletres[i] in ["a", "e", "i", "o", "u"]: print(lletres[i] + " ")
+    if lletres[i] in ["a", "e", "i", "o", "u"]: print(lletres[i], end = " ")
     i += 1
